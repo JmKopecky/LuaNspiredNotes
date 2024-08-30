28d5c8c4a461b8ac4c919807cb921c35c43d3a9c
Joseph Kopecky <joseph.m.kopecky@gmail.com>
2024-08-29 20:02:15 -0500

    Created multipage and image compatibility

--- script.lua
+++ script.lua
@@ -1,6 +1,6 @@
 -- Lua code by Joseph Kopecky, math notes by GCM Mu Alpha Theta
 
-platform.apilevel = "2.0"
+platform.apilevel = "2.3"
 
 local screen = platform.window
 local width = screen:width()
@@ -37,13 +37,23 @@ end
 
 function addPage(textList)
     for i,v in ipairs(textList) do 
-        textPage = D2Editor.newRichText()
-        textPage:move(0, 0)
-        textPage:resize(width, height * 0.88)
-        textPage:setText(v)
-        textPage:setReadOnly(true)
-        textPage:setVisible(false)
-        table.insert(textArray, textPage)
+        if v ~= " " then
+            textPage = D2Editor.newRichText()
+            textPage:move(0.01 * width, 0)
+            textPage:resize(width * 0.99, height * 0.88)
+            textPage:setText(v)
+            textPage:setReadOnly(true)
+            textPage:setVisible(false)
+            table.insert(textArray, textPage)
+        else 
+            textPage = D2Editor.newRichText()
+            textPage:move(0, 0)
+            textPage:resize(1, 1)
+            textPage:setText(v)
+            textPage:setReadOnly(true)
+            textPage:setVisible(false)
+            table.insert(textArray, textPage)
+        end
     end
     isCurrentlyMultipage = true;
     pageRenderIndex = 1
@@ -112,30 +122,25 @@ function switchDisplayMode(mode)
     end
     
     if displayMode == "notes_equilateral" then
-        addText(width*0.01,height*0.01,width*0.98,height*0.3,"a=b=c=leg dimensions | A=B=C=60°")
-        addText(width*0.01,height*0.21,width*0.98,height*0.3,"h=altitude, K=area")
-        addText(width*0.01,height*0.41,width*0.98,height*0.3,"\\0el {h=((√(3))/(2))*a}")
-        addText(width*0.51,height*0.41,width*0.98,height*0.3,"\\0el {K=((√(3))/(4))*a^(2)}")
+        conversionList = {};
+        table.insert(conversionList, "a=b=c=leg dimensions | A=B=C=60°\nh=altitude, K=area\n\\0el {h=((√(3))/(2))*a}\n\\0el {K=((√(3))/(4))*a^(2)}")
+        table.insert(conversionList, " ")
+        addPage(conversionList)
     end
     
     if displayMode == "notes_isosceles" then
-        addText(width*0.01,height*0.01,width*0.98,height*0.15,"a,b,c=leg dimensions | A,B,C=angles")
-        addText(width*0.01,height*0.15,width*0.98,height*0.15,"h=altitude on side c | K=area")
-        addText(width*0.01,height*0.3,width*0.98,height*0.15,"A=B | a=b | 2A+C=180°")
-        addText(width*0.01,height*0.4,width*0.98,height*0.25,"\\0el {h=a*sin(A)=((c)/(2))*tan(A)}")
-        addText(width*0.01,height*0.6,width*0.98,height*0.25,"\\0el {c^(2)=4(a^(2)+h^(2))=4(1+sin^(2)(A))*a^(2)}")
-        addText(width*0.01,height*0.75,width*0.98,height*0.25,"\\0el {K=((1)/(2))*c*h=((1)/(4))*c^(2)*tan(A)}")
+        conversionList = {};
+        table.insert(conversionList, "a,b,c=leg dimensions | A,B,C=angles | h=altitude on side c | K=area | A=B | a=b | 2A+C=180°\n\\0el {h=a*sin(A)=((c)/(2))*tan(A)}\n\\0el {c^(2)=4(a^(2)+h^(2))=4(1+sin^(2)(A))*a^(2)}\n\\0el {K=((1)/(2))*c*h=((1)/(4))*c^(2)*tan(A)}")
+        table.insert(conversionList, " ")
+        addPage(conversionList)
     end
     
     if displayMode == "notes_scalene" then
-        addText(width*0.01,height*0.01,width*0.98,height*0.15,"a,b,c=leg dimensions, h=altitude on side c")
-        addText(width*0.01,height*0.15,width*0.98,height*0.15,"A,B,C,C'=angles, K=area")
-        addText(width*0.65,height*0.15,width*0.98,height*0.15,"A+B+C=180°")
-        addText(width*0.01,height*0.25,width*0.98,height*0.25,"\\0el {K=((1)/(2))bc*sin(A)}")
-        addText(width*0.65,height*0.25,width*0.98,height*0.25,"\\0el {((2K)/(P))=r}")
-        addText(width*0.01,height*0.45,width*0.98,height*0.25,"\\0el {h=a*sin(B)=b*sin(A)}")
-        addText(width*0.01,height*0.55,width*0.98,height*0.25,"Sines: \\0el {((a)/(sin(A)))=((b)/(sin(B)))=((c)/(sin(C)))=((c)/(sin(C')))=2R}")
-        addText(width*0.01,height*0.75,width*0.98,height*0.25,"Cosines: \\0el {c^(2)=a^(2)+b^(2)-2ab*cos(C)=a^(2)+b^(2)-2ab*cos(C')}")
+        conversionList = {};
+        table.insert(conversionList, "a,b,c=leg dimensions, h=altitude on side c\nA,B,C,C'=angles, K=area \, A+B+C=180°\n\\0el {K=((1)/(2))bc*sin(A)}\n\\0el {((2K)/(P))=r}\n\\0el {h=a*sin(B)=b*sin(A)}")
+        table.insert(conversionList, "Sines: \\0el {((a)/(sin(A)))=((b)/(sin(B)))=((c)/(sin(C)))=((c)/(sin(C')))=2R}\n\nCosines: \\0el {c^(2)=a^(2)+b^(2)-2ab*cos(C)=a^(2)+b^(2)-2ab*cos(C')}")
+        table.insert(conversionList, " ")
+        addPage(conversionList)
     end
     
     if displayMode == "notes_circleinscirctri" then
@@ -143,6 +148,9 @@ function switchDisplayMode(mode)
         table.insert(conversionList, "Equilateral:   \\0el {r=((a√(3))/(6))}  \n  \\0el {R=((a√(3))/(3))}  \n  \\0el {r=((R)/(2))}")
         table.insert(conversionList, "Right Tri:   \\0el {r=s-c=((a+b-c)/(2))}    \\0el {R=((c)/(2))}    \\0el {r=R(((a+b-c)/(c)))}    \\0el {a,b=((2r+c)/(2))±((√(8c^(2)-4(2r+c)^(2)))/(4))}\n\\0el {b=((2r(a-r))/(a-2r))}    \\0el {c=(a-2r)+((2r(a-r))/(a-2r))=a+b-2r}")
         table.insert(conversionList, "Scalene: \\0el {K=√(s(s-a)(s-b)(s-c))}  \\0el {s=((1)/(2))(a+b+c)}    \\0el {r=(s-c)*tan(((C)/(2)))=(((a+b-c))/(2))*tan(((C)/(2)))}  \\0el {R=((abc)/(4K))}   \\0el {r=4R*sin(((A)/(2)))*sin(((B)/(2)))*sin(((C)/(2)))}")
+        table.insert(conversionList, " ")
+        table.insert(conversionList, " ")
+        table.insert(conversionList, " ")
         addPage(conversionList)
     end
     
@@ -153,49 +161,46 @@ function switchDisplayMode(mode)
     
     if displayMode == "notes_inscribedpolygon" then
         addText(width*0.01,height*0.01,width*0.98,height*0.5,"Regular Polygons\ns=side length of polygon\na=apothem (center to midpoint of side)\nR=circumcircle radius")
-        addText(width*0.01,height*0.48,width*0.98,height*0.35,"\\0el {a=((s)/(2*tan(((180)/(n)))))}")
-        addText(width*0.351,height*0.51,width*0.98,height*0.25,"\\0el {a=R*cos(((180)/(n)))}")
-        addText(width*0.71,height*0.51,width*0.98,height*0.25,"\\0el {area=((a*p)/(2))}")
-        addText(width*0.71,height*0.751,width*0.98,height*0.25,"\\0el {perimeter=n*s}")
+        addText(width*0.01,height*0.45,width*0.35,height*0.35,"\\0el {a=((s)/(2*tan(((180)/(n)))))}")
+        addText(width*0.01,height*0.78,width*0.35,height*0.25,"\\0el {a=R*cos(((180)/(n)))}")
+        addText(width*0.71,height*0.51,width*0.35,height*0.25,"\\0el {area=((a*p)/(2))}")
+        addText(width*0.71,height*0.751,width*0.35,height*0.25,"\\0el {perimeter=n*s}")
     end
     
     if displayMode == "notes_powerpoint" then
-        addText(width*0.01,height*0.01,width*0.98,height*0.33,"Two chords intercepting at inner point E:  \\0el {AE*CE=BE*DE}")
-        addText(width*0.01,height*0.34,width*0.98,height*0.33,"One secant BD with chord CD connecting with one tangent AB:  \\0el {AB^(2)=BC*CD}")
-        addText(width*0.01,height*0.67,width*0.98,height*0.33,"Two secants AC and EC with chords AB and ED intercepting at outer point C:  \\0el {AB^(2)=BC*CD}")
-    end
-    
-    if displayMode == "notes_intarcsangles" then
-        addText(width*0.01,height*0.01,width*0.98,height*0.2,"Two lines originating from center:  \\0el {θ=a}")
-        addText(width*0.01,height*0.15,width*0.98,height*0.25,"Two lines originating from edge:  \\0el {θ=((a)/(2))}")
-        addText(width*0.01,height*0.4,width*0.98,height*0.33,"Two arcs formed by two intersecting chords:  \\0el {θ=((a+b)/(2))}")
-        addText(width*0.01,height*0.7,width*0.98,height*0.33,"Two arcs formed by two secant lines intersecting at endpoints:  \\0el {θ=((b-a)/(2))}")
+        addText(width*0.01,height*0.05,width*0.33,height*0.17,"\\0el {AE*CE=BE*DE}")
+        addText(width*0.38,height*0.01,width*0.33,height*0.17,"\\0el {AB^(2)=BC*CD}")
+        addText(width*0.70,height*0.01,width*0.30,height*0.17,"\\0el {AB^(2)=BC*CD}")
     end
     
     if displayMode == "notes_segmentssectors" then
-        addText(width*0.01,height*0.01,width*0.98,height*0.2,"R=radius, K=area, c=chord")
-        addText(width*0.01,height*0.15,width*0.98,height*0.25,"\\0el {c=2R*sin(((θ)/(2)))}")
-        addText(width*0.01,height*0.4,width*0.98,height*0.33,"\\0el {K(segment)=((1)/(2))R^(2)(θ-sin(θ)) (radian only)}")
-        addText(width*0.01,height*0.7,width*0.98,height*0.33,"\\0el {K(sector)=((1)/(2))R^(2)θ (radian only)}")
+        conversionList = {};
+        table.insert(conversionList, "R=radius, K=area, c=chord\n\\0el {c=2R*sin(((θ)/(2)))}\n\\0el {K(segment)=((1)/(2))R^(2)(θ-sin(θ)) (radian only)}\n\\0el {K(sector)=((1)/(2))R^(2)θ (radian only)}")
+        table.insert(conversionList, " ")
+        addPage(conversionList)
     end
     
     if displayMode == "notes_hemisphere" then
-        addText(width*0.01,height*0.01,width*0.98,height*0.23,"\\0el {Hemisphere: S=2πR^(2)=((π)/(2))D^(2)}")
-        addText(width*0.01,height*0.23,width*0.98,height*0.23,"\\0el {Hemisphere: T=3πR^(2)=((3π)/(4))D^(2)}")
-        addText(width*0.01,height*0.46,width*0.98,height*0.23,"\\0el {Hemisphere: V=((2)/(3))πR^(3)=((1)/(12))πD^(3)}")
-        addText(width*0.01,height*0.67,width*0.98,height*0.15,"\\0el {Sphere: S=4πR^(2)=πD^(2)}")
-        addText(width*0.01,height*0.8,width*0.98,height*0.23,"\\0el {Sphere: V=((4)/(3))πR^(3)=((1)/(6))πD^(3)}")
+        conversionList = {};
+        table.insert(conversionList, "\\0el {Hemisphere: S=2πR^(2)=((π)/(2))D^(2)}\n\\0el {Hemisphere: T=3πR^(2)=((3π)/(4))D^(2)}\n\\0el {Hemisphere: V=((2)/(3))πR^(3)=((1)/(12))πD^(3)}")
+        table.insert(conversionList, "\\0el {Sphere: S=4πR^(2)=πD^(2)}\n\\0el {Sphere: V=((4)/(3))πR^(3)=((1)/(6))πD^(3)}")
+        table.insert(conversionList, " ")
+        table.insert(conversionList, " ")
+        addPage(conversionList)
     end
     
     if displayMode == "notes_cylinders" then
-        addText(width*0.01,height*0.01,width*0.98,height*0.35,"Right Circular Cylinders:\n\\0el {S=2πRh  T=2πR(R+h)  V=πR^(2)h}")
-        addText(width*0.01,height*0.4,width*0.98,height*0.35,"Slant/Right Cylinders of Any Shape:\n\\0el {S=sC=((hC)/(sin(θ)))   A=A'*sin(θ)  V=hA'=sA")
+        conversionList = {};
+        table.insert(conversionList, "Right Circular Cylinders:\n\\0el {S=2πRh  T=2πR(R+h)  V=πR^(2)}\nSlant/Right Cylinders of Any Shape:\n \\0el {S=sC=((hC)/(sin(θ)))   A=A'*sin(θ)  V=hA'=sA")
+        table.insert(conversionList, " ")
+        addPage(conversionList)
     end
     
     if displayMode == "notes_conespyramids" then
         conversionList = {};
         table.insert(conversionList, "Square-Base Pyramid:\n\\0el {h=((a*tan(α))/(√(2)))  s=((h)/(sin(α)))=((a)/(√(2)*cos(α)))   S=((a^(2))/(cos(α)))*√(1+sin^(2)(α))   T=S+a^(2)   V=((a^(2)h)/(3))")
         table.insert(conversionList, "Right Circular Cone:\n\\0el {s=√(R^(2)+h^(2))=((h)/(cos(θ)))=((R)/(sin(θ)))   S=πRs   T=πR(R+s)   V=((π)/(3))R^(2)h}")
+        table.insert(conversionList, " ")
         addPage(conversionList)
     end
     
@@ -203,6 +208,7 @@ function switchDisplayMode(mode)
         conversionList = {};
         table.insert(conversionList, "\\0el {s=√((R₁-R₂)^(2)+h^(2))  =((h)/(cos(θ)))   =((R₁-R₂)/(sin(θ)))}\n\\0el {S=π(R₁+R₂)s  T=π[R₁^(2)+R₂^(2)+(R₁+R₂)s]}\n\\0el {V=((1)/(3))πh(R₁^(2)+R₂^(2)+R₁*R₂)}")
         table.insert(conversionList, "R1 = radius of lower base (bigger radius)\nR2 = radius of upper base (smaller radius)\ns = slant height    h = altitude / height\nθ = semicone angle (half of angle at the tip)\nS = lateral surface area\nT = total surface area\nV = volume")
+        table.insert(conversionList, " ")
         addPage(conversionList)
     end
     
@@ -212,21 +218,24 @@ function switchDisplayMode(mode)
     end
     
     if displayMode == "notes_rectsquares" then
-        addText(width*0.01,height*0.01,width*0.98,height*0.3,"a,b=side dimensions, p=perimeter, d=diagonal, K=area, All internal angles = 90°")
-        addText(width*0.01,height*0.35,width*0.98,height*0.3,"Rectangles:\n\\0el {a≠b  d=√(a^(2)+b^(2))  p=2(a+b)  K=ab}")
-        addText(width*0.01,height*0.7,width*0.98,height*0.3,"Square:\n\\0el {a=b  d=a√(2)  p=4a  K=a^(2)}")
+        conversionList = {};
+        table.insert(conversionList, "a,b=side dimensions, p=perimeter, d=diagonal, K=area, All internal angles = 90° \n Rectangles:\n\\0el {a≠b  d=√(a^(2)+b^(2))  p=2(a+b)  K=ab}\nSquare:\n\\0el {a=b  d=a√(2)  p=4a  K=a^(2)}")
+        table.insert(conversionList, " ")
+        addPage(conversionList)
     end
     
     if displayMode == "notes_trapezoids" then
-        addText(width*0.01,height*0.01,width*0.98,height*0.3,"a,b,s,t=side dimensions, a and b are parallel, h=altitude on side a, K=area")
-        addText(width*0.01,height*0.35,width*0.98,height*0.3,"\\0el {h=s*sin(A)      h=t*sin(B)}")
-        addText(width*0.01,height*0.7,width*0.98,height*0.3,"\\0el {K=((1)/(2))(a+b)*h}")
+        conversionList = {};
+        table.insert(conversionList, "a,b,s,t=side dimensions, a and b are parallel, h=altitude on side a, K=area\n\\0el {h=s*sin(A)      h=t*sin(B)}\n\\0el {K=((1)/(2))(a+b)*h}")
+        table.insert(conversionList, " ")
+        addPage(conversionList)
     end
 
     if displayMode == "notes_rhombusparallelogram" then
         conversionList = {};
-        table.insert(conversionList, "a,b=side dimensions, p=perimeter, c,d=diagonals,h=altitude on side a, K=area, A,B,C,D=interior angles\nRhombus: \\0el {A=C, B=D, a=b    A+B=180°  c=a√(2(1-cos(A)))=a√(2(1+cos(B)))  d=a√(2(1+cos(A)))=a√(2(1-cos(B)))  p=4a    h=b*sin(A)=b*sin(B)  K=ah=ab*sin(A)=ab*sin(B)}")
+        table.insert(conversionList, "a,b=side dimensions, p=perimeter, c,d=diagonals,h=altitude on side a, K=area, A,B,C,D=interior angles\n\\0el {Rhombus: A=C, B=D, a=b    A+B=180°  c=a√(2(1-cos(A)))=a√(2(1+cos(B)))  d=a√(2(1+cos(A)))=a√(2(1-cos(B)))  p=4a    h=b*sin(A)=b*sin(B)  K=ah=ab*sin(A)=ab*sin(B)}")
         table.insert(conversionList, "Parallelogram: \\0el {A=C, B=D, a≠b    A+B=180°  c=√(a^(2)+b^(2)-2ab*cos(A))=√(a^(2)+b^(2)+2ab*cos(B))  d=√(a^(2)+b^(2)+2ab*cos(A))=√(a^(2)+b^(2)-2ab*cos(B))  p=2(a+b)   h=b*sin(A)=b*sin(B)  K=ah=ab*sin(A)=ab*sin(B)}")
+        table.insert(conversionList, " ")
         addPage(conversionList)
     end
     
@@ -275,6 +284,108 @@ function switchDisplayMode(mode)
         table.insert(conversionList, "g = acceleration on earth ≈ -32.174 ft/s^(2)    \nRadius of Earth ≈ 3960 miles    \nMass of Earth ≈ 5.98×10^(24) kg    \n1+x+x^(2)+x^(3)+x^(4)+x^(5)+...=(1-x)^(-1)    \n(x^(2)<1)")
         addPage(conversionList)
     end
+end
+
+powerpoint1 = image.new(_R.IMG.innerlinescircle):copy(width * 0.25, height * 0.35)
+powerpoint2 = image.new(_R.IMG['1tancircle']):copy(width * 0.25, height * 0.45)
+powerpoint3 = image.new(_R.IMG.singlepointinnercircle):copy(width * 0.25, height * 0.45)
+inscribedPoly = image.new(_R.IMG.inscribedpolygon):copy(width * 0.25, height * 0.45)
+interceptArcsAngles = image.new(_R.IMG.interceptedarcsangles):copy(width, height)
+rectangle = image.new(_R.IMG.rectangle):copy(width * 0.8, height * 0.8)
+trapezoid = image.new(_R.IMG.trapezoid):copy(width * 0.8, height * 0.8)
+rhombparallel = image.new(_R.IMG.parallelogram):copy(width * 0.8, height * 0.8)
+equitri = image.new(_R.IMG.equitri):copy(width * 0.8, height * 0.8)
+isotri = image.new(_R.IMG.isotri):copy(width * 0.5, height * 0.8)
+scaletri = image.new(_R.IMG.scaltri):copy(width * 0.8, height * 0.8)
+segmentsector = image.new(_R.IMG.segmentsectors):copy(width * 0.8, height * 0.35)
+hemisphere = image.new(_R.IMG.hemisphere):copy(width * 0.6, height * 0.8)
+sphere = image.new(_R.IMG.sphere):copy(width * 0.6, height * 0.8)
+cylinder = image.new(_R.IMG.cylinder):copy(width * 0.8, height * 0.8)
+cones = image.new(_R.IMG.conepyramid):copy(width * 0.8, height * 0.8)
+frustum = image.new(_R.IMG.frustum):copy(width * 0.4, height * 0.8)
+circleequi = image.new(_R.IMG.circleequitri):copy(width * 0.6, height * 0.8)
+circleright = image.new(_R.IMG.rightequitri):copy(width * 0.6, height * 0.8)
+circlescalene = image.new(_R.IMG.scalequitri):copy(width * 0.6, height * 0.8)
+
+
+function on.paint(gc)
+    gc:setColorRGB(0,0,0)
+    gc:setPen("thin", "smooth")
+    
+    if displayMode == "notes_powerpoint" then
+        gc:drawImage(powerpoint1, width * 0.25 * 0.25, height * 0.5)
+        gc:drawImage(powerpoint2, width * 0.25 + width * 0.25 * 0.25 * 2, height * 0.40)
+        gc:drawImage(powerpoint3, width * 0.25 * 2 + width * 0.25 * 0.25 * 3, height * 0.40)
+    end
+    
+    if displayMode == "notes_inscribedpolygon" then
+        gc:drawImage(inscribedPoly, width * 0.4, height * 0.55)
+    end
+    
+    if displayMode == "notes_intarcsangles" then
+        gc:drawImage(interceptArcsAngles, 0, 0)
+    end
+    
+    if displayMode == "notes_rectsquares" and pageRenderIndex == 2 then
+        gc:drawImage(rectangle, width * 0.05, height * 0.05)
+    end
+    
+    if displayMode == "notes_trapezoids" and pageRenderIndex == 2 then
+        gc:drawImage(trapezoid, width * 0.05, height * 0.05)
+    end
+    
+    if displayMode == "notes_rhombusparallelogram" and pageRenderIndex == 3 then
+        gc:drawImage(rhombparallel, width * 0.05, height * 0.05)
+    end
+    
+    if displayMode == "notes_equilateral" and pageRenderIndex == 2 then
+        gc:drawImage(equitri, width * 0.05, height * 0.05)
+    end
+    
+    if displayMode == "notes_isosceles" and pageRenderIndex == 2 then
+        gc:drawImage(isotri, width * 0.05, height * 0.05)
+    end
+    
+    if displayMode == "notes_scalene" and pageRenderIndex == 3 then
+        gc:drawImage(scaletri, width * 0.05, height * 0.05)
+    end
+    
+    if displayMode == "notes_segmentssectors" and pageRenderIndex == 2 then
+        gc:drawImage(segmentsector, width * 0.05, height * 0.25)
+    end
+    
+    if displayMode == "notes_hemisphere" and pageRenderIndex == 3 then
+        gc:drawImage(hemisphere, width * 0.05, height * 0.05)
+    end
+    if displayMode == "notes_hemisphere" and pageRenderIndex == 4 then
+        gc:drawImage(sphere, width * 0.05, height * 0.05)
+    end
+    
+    if displayMode == "notes_cylinders" and pageRenderIndex == 2 then
+        gc:drawImage(cylinder, width * 0.05, height * 0.05)
+    end
+    
+    if displayMode == "notes_conespyramids" and pageRenderIndex == 3 then
+        gc:drawImage(cones, width * 0.05, height * 0.05)
+    end
+    
+    if displayMode == "notes_frustumrightcirccone" and pageRenderIndex == 3 then
+        gc:drawImage(frustum, width * 0.05, height * 0.05)
+    end
+    
+    if displayMode == "notes_circleinscirctri" and pageRenderIndex == 4 then
+        gc:drawImage(circleequi, width * 0.05, height * 0.05)
+    end
+    if displayMode == "notes_circleinscirctri" and pageRenderIndex == 5 then
+        gc:drawImage(circleright, width * 0.05, height * 0.05)
+    end
+    if displayMode == "notes_circleinscirctri" and pageRenderIndex == 6 then
+        gc:drawImage(circlescalene, width * 0.05, height * 0.05)
+    end
+end
+
+function on.resize(gc)
+    screen:invalidate()
 end
 
 
